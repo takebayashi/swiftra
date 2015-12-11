59b5d21649180b404d761315cb66b55761806a73
Shun Takebayashi <shun@takebayashi.asia>
2015-12-11 23:30:40 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "swiftra",
     dependencies: [
-        .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 0, 3))
+        .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 0, 4))
     ]
 )
