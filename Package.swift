6b8e3c9d81268c6237706849e7aac7f2f1ce1cab
Shun Takebayashi <shun@takebayashi.asia>
2016-02-06 23:08:35 +0900

    Add dependency for Nest

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "swiftra",
     dependencies: [
+        .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 0, 7))
     ]
 )
